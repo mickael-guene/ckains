c5b8586879de67ba6aaa6dfe424f82e2458025f6
Mickael Guene <mickael.guene@st.com>
2015-06-12 09:51:02 +0200

    Add -B and -M options

--- src/options.c
+++ src/options.c
@@ -10,7 +10,10 @@ struct config config;
 static struct option long_options[] = {
     {"rootfs",          required_argument, NULL, 'r'},
     {"root-id",         no_argument, NULL, '0'},
-    {"bind",            required_argument, NULL, 'b'}
+    {"bind",            required_argument, NULL, 'b'},
+    {"mount",           required_argument, NULL, 'm'},
+    {"bind-elsewhere",  required_argument, NULL, 'B'},
+    {"mount-elsewhere", required_argument, NULL, 'M'}
 };
 
 static void setup_default_config(struct config *config)
@@ -33,7 +36,7 @@ int parse_options(int argc, char **argv)
     int opt;
 
     setup_default_config(&config);
-    while((opt = getopt_long(argc, argv, "+r:0b:", long_options, NULL)) != -1) {
+    while((opt = getopt_long(argc, argv, "+r:0b:m:B:M:", long_options, NULL)) != -1) {
         switch(opt) {
             case 'r':
                 config.rootfs = optarg;
@@ -41,8 +44,22 @@ int parse_options(int argc, char **argv)
             case '0':
                 config.is_root_id = 1;
                 break;
+            case 'm':
             case 'b':
                 append_mount_point(&config, optarg, optarg);
+                break;
+            case 'M':
+            case 'B':
+                /* As getopt has no support for multi parameter options we do
+                 * some operation that are dependent of implementation and so
+                 * fragile. incrementing optind to jump to next option is not
+                 * sure to work everywhere. Other solution is to implement a
+                 * custom parser ......
+                 */
+                if (argv[optind][0] == '-')
+                    return -1;
+                append_mount_point(&config, optarg, argv[optind]);
+                optind++;
                 break;
             default:
                 return -1;
